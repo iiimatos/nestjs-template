c57c31ccca05e3c98161c881dce59922387749f8
iiimatos <ivanmatosvillar@gmail.com>
2024-03-23 22:43:51 -0400

    feat: integrated prisma

---
+++ src/users/users.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+}
